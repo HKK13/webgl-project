583adc4ed24c31a37464af0b2e4ce279e1045eef
Yagiz Nizipli <yagiznizipli@me.com>
2015-12-24 23:55:32 +0200

    Add sockets

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use('/game', express.static('./src'));
 
 app.get('/', (req, res) => {
     res.json({'hello': 'world'});
 });
-
-var server = app.listen(3000, () => {
-    console.log('Listening at port', server.address().port);
+io.on('connection', (socket) => {
+    console.log('User connected with id', socket.id);
 });
+
+server.listen(3000);
